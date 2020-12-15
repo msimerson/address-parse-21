4d717977213350cecff30beea532963ff4c5d2bb
Matt Simerson <matt@tnpi.net>
2020-12-14 17:43:29 -0800

    index: convert to es6 class

--- index.js
+++ index.js
@@ -1,94 +1,94 @@
 'use strict';
 
 const punycode = require('punycode');
-
-const nearley = require('nearley');
+const nearley  = require('nearley');
 
 const grammar = nearley.Grammar.fromCompiled(require('./grammar.js'));
-
 grammar.start = 'main';
 
 // a class encapsulating an email address per RFC-5321
 
-function Address (user, host) {
-    if (typeof user === "object" && user.original) {
-        // Assume constructing from JSON parse
-        for (const k in user) {
-            this[k] = user[k];
+class Address {
+    constructor (user, host) {
+        if (typeof user === "object" && user.original) {
+            // Assume constructing from JSON parse
+            for (const k in user) {
+                this[k] = user[k];
+            }
+            return this;
         }
-        return this;
-    }
-    if (!host) {
-        this.original = user;
-        this.parse(user);
-    }
-    else {
-        this.original = `${user}@${host}`;
-        this.user = user;
-        this.original_host = host;
+        if (!host) {
+            this.original = user;
+            this.parse(user);
+        }
+        else {
+            this.original = `${user}@${host}`;
+            this.user = user;
+            this.original_host = host;
 
-        if (/[\u00C0-\uFFFF]/.test(host)) {
+            if (/[\u00C0-\uFFFF]/.test(host)) {
+                this.is_utf8 = true;
+                host = punycode.toASCII(host);
+            }
+
+            this.host = host.toLowerCase();
+        }
+    }
+
+    parse (addr) {
+        // empty addr is ok
+        if (addr === '' || addr === '<>') {
+            this.user = '';
+            this.host = '';
+            return;
+        }
+
+        // bare postmaster is permissible: RFC-5321 (4.1.1.3)
+        const addrlc = addr.toLowerCase();
+        if (addrlc === 'postmaster' || addrlc == '<postmaster>') {
+            this.user = 'postmaster';
+            this.host = '';
+            return;
+        }
+
+        const parser = new nearley.Parser(grammar);
+
+        parser.feed(addr);
+        const result = parser.results[0][0];
+
+        let domainpart = result.domain;
+        this.original_host = domainpart;
+
+        if (/[\u0100-\uFFFF]/.test(domainpart)) {
             this.is_utf8 = true;
-            host = punycode.toASCII(host);
+            domainpart = punycode.toASCII(domainpart);
         }
 
-        this.host = host.toLowerCase();
-    }
-}
-
-Address.prototype.parse = function (addr) {
-    // empty addr is ok
-    if (addr === '' || addr === '<>') {
-        this.user = '';
-        this.host = '';
-        return;
+        this.host = domainpart.toLowerCase();
+        this.user = result.local_part;
     }
 
-    // bare postmaster is permissible: RFC-5321 (4.1.1.3)
-    const addrlc = addr.toLowerCase();
-    if (addrlc === 'postmaster' || addrlc == '<postmaster>') {
-        this.user = 'postmaster';
-        this.host = '';
-        return;
+    isNull () {
+        return this.user ? 0 : 1;
     }
 
-    const parser = new nearley.Parser(grammar);
+    format (use_punycode) {
+        if (this.isNull()) return '<>';
 
-    parser.feed(addr);
-    const result = parser.results[0][0];
-
-    let domainpart = result.domain;
-    this.original_host = domainpart;
-
-    if (/[\u0100-\uFFFF]/.test(domainpart)) {
-        this.is_utf8 = true;
-        domainpart = punycode.toASCII(domainpart);
+        return `<${this.address(null, use_punycode)}>`;
     }
 
-    this.host = domainpart.toLowerCase();
-    this.user = result.local_part;
-}
-
-Address.prototype.isNull = function () {
-    return this.user ? 0 : 1;
-}
-
-Address.prototype.format = function (use_punycode) {
-    if (this.isNull()) return '<>';
-
-    return `<${this.address(null, use_punycode)}>`;
-}
-
-Address.prototype.address = function (set, use_punycode) {
-    if (set) {
-        this.original = set;
-        this.parse(set);
+    address (set, use_punycode) {
+        if (set) {
+            this.original = set;
+            this.parse(set);
+        }
+        return (this.user || '') + (this.original_host ? `@${(use_punycode ? this.host : this.original_host)}` : '');
     }
-    return (this.user || '') + (this.original_host ? `@${(use_punycode ? this.host : this.original_host)}` : '');
-}
 
-Address.prototype.toString = function () {
-    return this.format();
+    toString () {
+        return this.format();
+    }
 }
 
 exports.Address = Address;
